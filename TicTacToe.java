46e11a693cf603306497db647c3022383848ee5f
rohitdighe19 <rohitdighe47@gmail.com>
2021-07-16 17:48:10 +0530

    solution of UC2

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,27 +1,54 @@
-public class TicTacToe {
-    public static void main(String[] args) {
-        char[] board = new char[10];
+import java.util.*;
+class Tic_Tac_Toe_Game implements Board
+{
+    char choice;
+    char player_choice;
+    char computer_choice;
+    Scanner sc=new Scanner(System.in);
 
-        System.out.println("Game Start!");
 
-        //Display the initial board
-        for(int i = 0; i < 9; i++) {
-
+    public void Creating_Board_UC1(){
+        char [] board={' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
+        System.out.println("Board with number position ");
+        for(int i=1;i<board.length;i++)
+        {
+            System.out.println(i+"|"+(++i)+"|"+(++i));
+            System.out.println("-+"+"-+-");
         }
-        showboard(board);
-
-
-
     }
-    //Display the board
-    public static void showboard(char[] board) {
-        System.out.println("\n" + "Displaying the Tic Tac Toe Board");
-        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
-        System.out.println("---------");
-        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
-        System.out.println("---------");
-        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
-
-
+    
+    public char Choose_X_O_UC2(){
+        System.out.println("Want you want X or O");
+        choice=sc.next().charAt(0);
+        if(choice=='X' || choice=='x')
+        {
+            player_choice=choice;
+            computer_choice='O';
+            System.out.println("Your choose  "+player_choice);
+            System.out.println("Computer choose "+computer_choice);
+        }
+        else if(choice=='O' || choice=='o')
+        {
+            player_choice=choice;
+            player_choice='X';
+            System.out.println("Your choose "+player_choice);
+            System.out.println("Computer choose "+computer_choice);
+        }
+        else
+            System.out.println("invalid char");
+        return player_choice;
     }
+
+    public static void main(String[] args)
+    {
+        Tic_Tac_Toe_Game t1=new Tic_Tac_Toe_Game();
+        t1.Creating_Board_UC1();
+        t1.Choose_X_O_UC2();
+    }
+}
+
+interface Board
+{
+    void Creating_Board_UC1();
+    char Choose_X_O_UC2();
 }
